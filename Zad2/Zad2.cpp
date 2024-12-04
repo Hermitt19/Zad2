74d6249eed44f384be9826fab9726c562c8c0267
Hermitt19 <hermitt5312@gmail.com>
2024-12-04 23:30:11 +0300

    Update Zad2.cpp

--- Zad2/Zad2.cpp
+++ Zad2/Zad2.cpp
@@ -1,23 +1,73 @@
-﻿#include <iostream>
-using namespace std;
+﻿//#include <iostream>
+//using namespace std;
+//
+//void FindOddOrEvenNumbers(int N, int OddOrNot)
+//{
+//	for (int i = OddOrNot; i <= N; i += 2)
+//	{
+//		cout << i << "\n";
+//	}
+//}
+//
+//int main()
+//{
+//	int N;
+//	cout << "Enter yout number:";
+//	cin >> N;
+//
+//	cout << "Even numbers are: \n";
+//	FindOddOrEvenNumbers(N, 2);
+//
+//	cout << "Odd Numbers are: \n";
+//	FindOddOrEvenNumbers(N, 1);
+//}
 
-void FindOddOrEvenNumbers(int N, int OddOrNot)
-{
-	for (int i = OddOrNot; i <= N; i += 2)
-	{
-		cout << i << "\n";
-	}
-}
+
+
+
+#include <iostream> 
+#include <time.h>
+
+using namespace std;
 
 int main()
 {
-	int N;
-	cout << "Enter yout number:";
-	cin >> N;
+    const int N = 5;
+    int array[N][N]{};
 
-	cout << "Even numbers are: \n";
-	FindOddOrEvenNumbers(N, 2);
 
-	cout << "Odd Numbers are: \n";
-	FindOddOrEvenNumbers(N, 1);
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            array[i][j] = i + j;
+        }
+    }
+
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    time_t t = time(NULL);
+    struct tm buf;
+    localtime_s(&buf, &t);
+    int day = buf.tm_mday;
+
+
+
+
+
+    int sum = 0;
+    for (int j = 0; j < N; ++j)
+    {
+        sum += array[day % N][j];
+    }
+    cout << "SUM: " << sum << '\n';
+
+    return 0;
 }
